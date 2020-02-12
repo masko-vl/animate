02c73a7a103408c25ba23f565fa380aa7c4a75a9
masko-vl <58594257+masko-vl@users.noreply.github.com>
2020-02-12 15:36:08 +0100

    Displayed api events in app.js for future use in components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import Header from './components/Header/Header.js'
 import FiltersNavbar from './components/FiltersNavbar/FiltersNavbar.js'
@@ -6,13 +7,49 @@ import CalendarNav from './components/CalendarNav/CalendarNav.js'
 import EventList from './components/EventList/EventList.js'
 import EventMap from './components/EventMap/EventMap.js'
 import EventDetails from './components/EventDetails/EventDetails.js'
+import { render } from 'react-dom';
 
 
+class App extends Component {
+  state = {
+    data: {},
+    isLoading: true
+    
+}
+getDate = () => {
+  //SELECT THE CURRENT DATE
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
 
+  today = yyyy + '-' + mm + '-' + dd;
+  console.log(today);
+  return today;
+}
+async componentDidMount(){
+    //insert the current date in the url
+  const {data} = await axios(`https://analisi.transparenciacatalunya.cat/resource/rhpv-yr4f.json?$where=data_inici%3E=%22${this.getDate()}%22`)
+        console.log(data)
+        this.setState({
+          data, 
+          isLoading: false
+        })
+        
+  }
 
-function App() {
-  return (
+  render(){
+    return (
     <div className="">
+      {/* {this.state.isLoading
+      ? <h1>LOADING...</h1>
+      :<div><ul>
+        {this.state.data.map(function(event, i){
+          return(
+            <li key={i}>{event.denominaci}</li>
+          )
+        })} </ul>
+      </div>}  */}
       <Header/>
       <FiltersNavbar/>
       <CalendarNav/>
@@ -20,10 +57,10 @@ function App() {
       <EventMap/>
       <EventDetails/>
      
-      <section>DETAILS EVENT</section>
+      
 
     </div>
-  );
+  );}
 }
 
 export default App;
--- src/components/FiltersNavbar/FiltersNavbar.js
+++ src/components/FiltersNavbar/FiltersNavbar.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './FiltersNavbar.css'
  function Navbar(){
      return(
-        <div>FILTERS NAVBAR DISPLAY</div>
+        <div className='containerFiltersNav'>
+        <h4>Choose your preferences!</h4> 
+        <form action="" className='formFilters'>
+            <label>WHERE:</label>
+            <select>
+                <option>City</option>
+                <option>Barcelona</option>
+            </select>
+            <br></br>
+            <label>WHAT TYPE OF EVENT:</label>
+            <select>
+                <option>Type</option>
+                <option>Culture</option>
+                <option>Sports</option>
+                <option>Art</option>
+                <option>Music</option>
+            </select>
+            <br></br>
+            <label>WHEN:</label>
+            <input type='date'></input>
+            <br></br>
+            <button>ANÍMATE!</button>
+        </form>
+        </div>
      )
  }
 
