c9ea7e2e492b76c3d45e3463de3ccc4fdf8c5d06
Julie Brosse <privatejuliedev@gmail.com>
2020-02-13 16:18:24 +0100

    dates displayed with all details, hour included

--- src/components/CalendarNav/CalendarNav.js
+++ src/components/CalendarNav/CalendarNav.js
@@ -1,69 +1,34 @@
 import React, {Component} from 'react';
-// import './CalendarNav.css'
 
+const getDateArray = (start, end) => {
+    var arr = [];
+    var dt = new Date(start);
+    while (dt <= end) {
+        arr.push(new Date(dt));
+        dt.setDate(dt.getDate() + 1);
+    }
+    return arr;
+}
 
-// class CalendarNav extends Component {
-//     state = {
-//         startDate: new Date("2017-10-01"), // today by default or date chosen by the user
-//         endDate: new Date("2017-14-04"), //date.today + 6 months
-//     }
+const DisplayedDates = (startDate, endDate) => {
+    var dateArr = getDateArray(startDate, endDate);
+    for (var i = 0; i < dateArr.length; i++) {
+    document.write("<p>" + dateArr[i] + "</p>");
+}}
 
-//     DateArray = (startDate, endDate) => {
-//         var arr = [];
-//         var date = new Date(startDate);
-//         while (date <= endDate) {
-//             arr.push(new Date(startDate));
-//             startDate.setDate(startDate.getDate() + 1);
-//         }
-//         return arr;
-//     }
-
-//     arrayCalendar = () => {for (var i = 0; i < this.DateArray.length; i++) {
-//         console.log("<p>" + this.DateArray[i] + "</p>");
-//     }}
-
-
- class CalendarNav extends Component {
+class CalendarNav extends Component {
     state = {
-        startDate: new Date("2017-10-01"), // today by default or date chosen by the user
-        endDate: new Date("2017-14-04"), //date.today + 6 months
+        startDate: new Date("2017-10-01"), // today by default
+        endDate: new Date("2017-12-04"), //date.today + 6 months
     }
 
-    getDateArray = () => {
-        const arr = [];
-        const dt = new Date(this.state.startDate);
-        while (dt <= this.state.endDate) {
-            arr.push(new Date(dt));
-            dt.setDate(dt.getDate() + 1);
-        }
-        for (var i = 0; i < this.length; i++) {
-         this[i]}
-    }
-    
-    // array = () => {for (var i = 0; i < this.getDateArray().length; i++)
-    //     "<p>" + this.getDateArray()[i] + "</p>"}
-
-// Output
     render() {
         return(
-            <p>
-                {getDateArray()}
-            </p>
+            <div>
+                {DisplayedDates(this.state.startDate,this.state.endDate)}
+            </div>
         )
     }
 };
 
 export default CalendarNav;
-
-
-{/* //         return(
-//             <p>arrayCalendar</p>
-//         )
-//     }
-
-// } */}
-
-
-{/* <div className="calendarSwiftContainer">WEEK DAYS FILTERS BUTTON</div> */}
-
-// export default CalendarNav  
