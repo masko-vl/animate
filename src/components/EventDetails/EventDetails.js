9745f09a57a33384476005e87561490a3f25119f
Sandra <alecsandra.serediuc@gmail.com>
2020-02-17 12:19:42 +0200

    Added icons to the navbar and did the event details draft card

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -1,10 +1,102 @@
 import React from 'react';
- function EventDetails(){
-     return(
-        <section>MAPEVENTS<div>little event display</div></section>
-     )
- }
+import '../EventDetails/EventDetails.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import EuroIcon from '@material-ui/icons/Euro';
+import PublicIcon from '@material-ui/icons/Public';
+import CategoryIcon from '@material-ui/icons/Category';
+
+const useStyles = makeStyles({
+  root: {
+    width: "auto",
+    border: "solid 2px grey",
+    
+
+    
+    // change the width to the auto from 345
+
+  },
+});
+
+export default function EventDetails() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Event details"
+          height="auto"
+          image="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_960_720.jpg"
+          title="Event details"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">Event Name</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Palau de la Música Catalana</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          <LocationOnIcon fontsize ="small" color="action"/>C/ Palau de la Música, 4-6, 08003 Barcelona
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          <WatchLaterIcon fontzise='small' color='action' />
+              18.00 - 20.00
+               </Typography>
+          <Typography variant="body2" color="textSecondary" component="p"><CategoryIcon />music
+          </Typography>
+         <div><Typography variant="body2" color="textSecondary" component="p">
+          <EuroIcon />20 $</Typography></div> 
+          <Typography variant="body2" color="textSecondary" component="p"><PublicIcon />www.odod.com</Typography>
+         <Typography variant="body2" component="p">
+         Event details paragraph
+         </Typography>     
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
 
 
+//  function EventDetails(){
+//      return(
+//          <div className='events-details'>
+//            <h2 className='title'>Title</h2>
+//            <p>startTime endTime</p>
+//            <p>placename</p>
+//            <p>address</p>
+//            <p>description</p>
+//            <p>event tipe</p>
+//            <p>url</p>
+//          </div>
+//      )
+//  }
 
-export default EventDetails;
+
+/* state = {
+    title: "title1",
+    description: "lorem ipsum event 1",
+    startTime: "08:00",
+    endTime: "21:00",
+    placeName: "",
+    : "12 carrer d'en Roca, 08002 Barcelona",
+    distance: "2km",
+    eventType : "sport"
+url evento: */ 
+
+// export default EventDetails;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import '../Header/Header.css';
+import HomeIcon from '@material-ui/icons/Home';
+import MapIcon from '@material-ui/icons/Map';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import FilterListIcon from '@material-ui/icons/FilterList';
+// import Logo from './Logo/Logo.svg'
 
  const Header= props =>(
     <header className='header'>
         <nav className='header_navigation'>
-        <div></div>
-        <div className='header_logo'><a href = "/">LOGO: ANIMATE!</a></div>
+        <div className='logo'><a href='/'>Animate</a></div>
+        {/* <div className='header_logo'>
+        <img src={Logo} width="auto" /></div> */}
         <div className='space'/>
         <div className='header_items'>
             <ul>
-                <li><a href='/'>Map</a></li>
-                <li><a href='/'>Calendar</a></li>
-                <li><a href='/'>Filters</a></li>
+                <li><a href='/'><HomeIcon fontSize="medium" color="action" /></a></li>
+                <li><a href='/'><MapIcon fontsize ="large" color="action"/> </a></li>
+                <li><a href='/'><DateRangeIcon fontsize ="large" color="action"/> </a></li>
+                <li><a href='/'><FilterListIcon fontsize ="large" color="action"/> </a></li>
             </ul>
         </div>
         </nav>
     </header>
      );
- 
-
 
 
 export default Header;
