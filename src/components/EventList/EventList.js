0d6c0104abf1c1a84be05611f514463bf9113ac6
Julie Brosse <privatejuliedev@gmail.com>
2020-02-25 11:19:16 +0100

    sharedFunctions.js file created. All information needed in eventslist catched

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,14 +1,13 @@
 import React, {Fragment} from 'react';
-import {eventsCategories} from '../images/images.js';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import EventDetails from './../EventDetails/EventDetails.js';
 import { render } from '@testing-library/react';
+import {minPrice, categoryAvatar, undefinedCategoryAvatar} from './../../sharedFunctions.js'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,55 +21,15 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-// This function gets the smallest number from a string containing all kinds of characters. We'll use it to return the smallest price of an event, that sets in the api price sentence.
-const minPrice = (sentence) => {
-  const array = sentence.split(" ")
-  const numbersArray = []
-  let result = ""
-  for (let i = 0; i < array.length; i++) {
-    if (!isNaN(Number(array[i])) == true) {
-      numbersArray.push(Number(array[i]));
-    }  
-    if (numbersArray.length > 0) {
-      result = " from " + Math.min(...numbersArray) + "€"
-    } else {
-      result = "Click for more Info"
-    };
-
-  }
-  return result 
-};
-
-// GETTING THE IMAGE CORREPONDING TO THE API NAME OF CATHEGORY
-const categoryAvatar = (apiCategory) => {
-  const array = apiCategory.split("/")
-// get the name of the category from the api
-  const category = array[array.length - 1]
-  console.log(category)
-
-// Choosing the corresponding image in the images.js file. !! Each name of image should take the exact same name as the api cathegory name to make them match !!
-  // return <Avatar alt = {category} src={eventsCategories[category]} />
-  const categoryImage = eventsCategories[category]
-  return <Avatar alt ={category} src={categoryImage} />
-};
-
-const undefinedCategory = () => {
-  return <Avatar alt ="event" src="./images/undefined_category.jpg" />
-}
-
 const EventList = (props) => { 
   render()
     return(
       props.apiFiltered.map(x => 
-
-
-        // Do we keep this component format?
       <Fragment>
         <List className={useStyles.root}>
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
-            {/* Choosing image according to the category. All images are registered in "images" and have the name of the list category (as we need to get the category names of the api >> being sure that the filters names don't have been modified !!!*/}
-            {x.tags_categor_es ? categoryAvatar(x.tags_categor_es) : undefinedCategory()}
+            {x.tags_categor_es ? categoryAvatar(x.tags_categor_es) : undefinedCategoryAvatar()}
             </ListItemAvatar>
             <ListItemText
               primary={x.denominaci}
@@ -85,8 +44,6 @@ const EventList = (props) => {
                   </Typography>
                   {x.descripcio.slice(0, 100) + "..."}<br/> 
                   {x.data_inici == x.data_fi ? "" : "until " + new Date(x.data_fi.toString()).toString().slice(0, 9)} <br />
-
-
                   {x.horari ? x.horari.slice(0, 50) + "[...]" : "Click to get time table"}<br/> 
                   {x.entrades ? "Price :" + minPrice(x.entrades): "free"}<br/>  
                 </React.Fragment>
