3cd9c1491bdca200239b0676d9ec05cbf9f79904
masko-vl <58594257+masko-vl@users.noreply.github.com>
2020-02-27 01:01:06 +0100

    Added form control for no selected sity and if there's no events

--- src/components/FiltersNavbar/FiltersNavbar.js
+++ src/components/FiltersNavbar/FiltersNavbar.js
@@ -21,7 +21,9 @@ export default class FiltersNavbar extends Component {
     category:'all',//by default we are going to have al categoriesif you don't change it
     date: todayDate(), /*today by default */
     data: [],
-    showFilters: true
+    showFilters: true,
+    alertEmptyEvents:false, /*state to show an display if the events for that day doesent exist, this one and the next are changed in chooseFilters botton function */
+    alertEmptyCity: false /*state to show a alert when the city it isn't choose */
   }
   saveCity=(e)=>{
     const city = e.target.value
@@ -30,6 +32,7 @@ export default class FiltersNavbar extends Component {
     this.setState({
       //return value of the city
       city: e.target.value,
+      alertEmptyCity: false, /*if you change the city after having an alert of the oligatority, hide the alert fter change */
       data:updateFilteredApi(this.props.dataApi, city, category, dateEvent)
       //IS NEEDED TO PASS ALL THE CITIES OF THE DATA API
     },()=>{showEventsCounter(this.state.data)})  
@@ -64,30 +67,27 @@ export default class FiltersNavbar extends Component {
     const city = this.state.city
     const dateEvent = this.state.date
     const category = this.state.category
-    /* 
+    
     if(city=== ''){
-     alert('sasasasas')
-    }else{ */
+      /*chenge to true to display in the render a message for the user to remind to put a city */
+      this.setState({
+        alertEmptyCity: true})
+       
+    }else{ 
     //create a loop in the events api array and pass if statement for select the events and display diferent errors
     this.setState({
       data:updateFilteredApi(this.props.dataApi, city, category, dateEvent),
+
     }, () => {
       if (this.state.data.length > 0) {
         this.setState({
-      showFilters: false
+      showFilters: false,
+      
     })
     } else {
-      alert("there arent results, please change the filters")
-    }
-    }
-    )
-   //save in state al the events filtered
-     //console.log(this.state.data)
-   /*THINGS TO BE DONE:
-      -CHANGE IN A BETER WAY THE ALERTS CAUGHT
-      -CHANGE THE WAY OF DISPLAYS THE ALERTS AND AD THE MOMENT WHEN THERE'S NO EVENTS IN THIS DAY
-      -EXTRA:TO SHOW THE NUMBER OF EVENTS FILTERED BY THE FILTERS FOR MORE INFORMATION POR THE USER (IT'S NEEDED TO CHANGE THE LOGIC OF THE FILTES, SO IT SAVE THE INFO WHILE YOU ARE CHANGING THE FILTERS, NOT ONLY AT THE END ONCLICK BUTTON)
-      */
+      /*if there arent event also show a display in the render and don't go to events list */
+      this.setState({alertEmptyEvents: true})
+    }})}
   }
   updateEventCalendar=(e)=>{
     //convert to api format
@@ -120,10 +120,12 @@ export default class FiltersNavbar extends Component {
                 <DatePicker date2={this.state.date} changeDate={this.saveDate} dateCut={this.props.dateCut}/>
             </MuiPickersUtilsProvider>
         </Grid>
-        <Grid  item xs={12}>{this.chooseFilters}</Grid>
-
+        {/*displayed alerts for the caught errors IT WILL BE GOOD TO PUT IT IN RED*/}
+        <Grid  item xs={12}>{this.state.alertEmptyCity? <p>please select a city for displaying results</p>: <p></p>}{this.state.alertEmptyEvents? <p>there arent results, please change the filters</p>: <p></p>}</Grid>
+        {/*display counter of events while selecting filters*/}
         <Grid  item xs={12}>{showEventsCounter(this.state.data)} corresponding event(s)</Grid>
         <Grid item xs={12}><ButtonForm chooseFilters={this.chooseFilters}/></Grid> 
+
       </Grid>
     : <CalendarNav showFilters={this.showFilters} apiFiltered={this.state.data} updateEventCalendar={this.updateEventCalendar} getDateArray={getDateArray(new Date(), new Date(this.props.dateCut))}/>}
     </div>
