97b80ed4e0f3079fa70ebc7e1ab4fda614b45cc1
Sandra <alecsandra.serediuc@gmail.com>
2020-02-27 23:28:48 +0100

    Added the function for replacing some specific letters from the citie's words

--- src/sharedFunctions.js
+++ src/sharedFunctions.js
@@ -109,4 +109,10 @@ export const undefinedCategoryAvatar = () => {
     return data.length
   } 
 
-
+  function makeItBeautiful(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' ').replace(" L ", " l'").replace("L ", "").replace(" A ", " a ").replace(" D ", " ")
+  }
