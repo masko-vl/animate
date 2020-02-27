43aad43d44fb56a4a22c386266290e37c56d0596
Sandra <alecsandra.serediuc@gmail.com>
2020-02-27 23:26:00 +0100

    Chaging the name of the cities with upercase and replacing the ls

--- src/components/FiltersNavbar/SelectCity/SelectCity.js
+++ src/components/FiltersNavbar/SelectCity/SelectCity.js
@@ -5,7 +5,7 @@ import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-
+import {makeItBeautiful} from '../.'
 
 
 const useStyles = makeStyles(theme => ({
@@ -18,7 +18,6 @@ const citySelect=(value)=>{
   let arrayCities = value.split('/')
   let city = arrayCities[arrayCities.length - 1]
   city = city.replace(/([-])/g," ")
-
   return city
 }
 
@@ -26,6 +25,18 @@ const citySelect=(value)=>{
 
 export default function SelectCity({changeCity, valueCities}) {
   const classes = useStyles();
+  let cityArr = [];
+  if(valueCities) {
+    cityArr = valueCities.map(city => {
+      return (
+        {
+          value: city,
+          displayText:makeItBeautiful(citySelect(city).charAt(0).toUpperCase() + citySelect(city).slice(1))
+        }
+      );
+    }) 
+  }
+  
   return (
 
     <div>
@@ -34,8 +45,8 @@ export default function SelectCity({changeCity, valueCities}) {
         <InputLabel htmlFor="grouped-select" >Select city</InputLabel>
         <Select defaultValue="" onChange={changeCity} input={<Input id="grouped-select"  required/>}>
         {/*show the city categories */}
-        {!valueCities? <MenuItem value={'barcelona'}>Barcelona</MenuItem>:valueCities.map((cityvalue, index)=>{
-            return(<MenuItem key={index} value={cityvalue}>{citySelect(cityvalue)}</MenuItem>)
+        {!valueCities? <MenuItem value={'barcelona'}>Barcelona</MenuItem>:cityArr.map((city, index)=>{
+            return(<MenuItem key={index} value={city.value}>{city.displayText}</MenuItem>)
           }) }      
         </Select>
       </FormControl>
