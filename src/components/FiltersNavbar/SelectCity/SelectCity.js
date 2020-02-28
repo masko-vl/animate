5aafcfaa96c8755140e059184f9cc095e9e9118c
Sandra <alecsandra.serediuc@gmail.com>
2020-02-29 00:16:05 +0100

    Added a function that deletes duplicates cities

--- src/components/FiltersNavbar/SelectCity/SelectCity.js
+++ src/components/FiltersNavbar/SelectCity/SelectCity.js
@@ -5,8 +5,9 @@ import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import {makeItBeautiful} from './../../../sharedFunctions'
-import {citySort} from './../../../sharedFunctions'
+import { makeItBeautiful } from './../../../sharedFunctions'
+import { citySort } from './../../../sharedFunctions'
+import { deleteDuplicatedCities } from './../../../sharedFunctions'
 
 
 const useStyles = makeStyles(theme => ({
@@ -15,49 +16,46 @@ const useStyles = makeStyles(theme => ({
     minWidth: 260,
   },
 }));
-const citySelect=(value)=>{
+const citySelect = (value) => {
   let arrayCities = value.split('/')
   let city = arrayCities[arrayCities.length - 1]
-  city = city.replace(/([-])/g," ")
+  city = city.replace(/([-])/g, " ")
   // console.log(city)
 
   return city
 }
 
-
-export default function SelectCity({changeCity, valueCities}) {
+export default function SelectCity({ changeCity, valueCities }) {
   const classes = useStyles();
   let cityArr = [];
-  
-  if(valueCities) {
+
+  if (valueCities) {
     cityArr = valueCities.map(city => {
       return (
         {
           value: city,
-          displayText:makeItBeautiful(citySelect(city).charAt(0).toUpperCase() + citySelect(city).slice(1))
+          displayText: makeItBeautiful(citySelect(city).charAt(0).toUpperCase() + citySelect(city).slice(1))
         }
       );
-    }) 
+    })
+    cityArr = deleteDuplicatedCities(cityArr, 'displayText') //call the function with our city array and the key name we need
     cityArr.sort(citySort('displayText'))
-    
   }
   console.log(cityArr)
-  // const finalDataFiltered = [...new Map(cityArr.displayText.map(event => [event.codi, event])).values()]
-  // console.log(finalDataFiltered)
-  
+
   return (
 
     <div>
-   
-      <FormControl  variant="outlined" className={classes.formControl}>
+
+      <FormControl variant="outlined" className={classes.formControl}>
         <InputLabel htmlFor="grouped-select" >Select city</InputLabel>
-        <Select defaultValue="" onChange={changeCity} input={<Input id="grouped-select"  required/>}>
-        {/*show the city categories */}
-        {!valueCities? <MenuItem value={'barcelona'}>Barcelona</MenuItem>:cityArr.map((city, index)=>{
-            return(<MenuItem key={index} value={city.value}>{city.displayText}</MenuItem>) 
-            
-          }) }  
- 
+        <Select defaultValue="" onChange={changeCity} input={<Input id="grouped-select" required />}>
+          {/*show the city categories */}
+          {!valueCities ? <MenuItem value={'barcelona'}>Barcelona</MenuItem> : cityArr.map((city, index) => {
+            return (<MenuItem key={index} value={city.value}>{city.displayText}</MenuItem>)
+
+          })}
+
         </Select>
       </FormControl>
     </div>
