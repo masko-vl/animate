375ef0bbecb8dcaccd7de901a87beb885fd1530a
Sandra <alecsandra.serediuc@gmail.com>
2020-02-29 19:29:22 +0100

    Added a function that filters the events that don't have lat and long

--- src/components/EventMap/EventMap.js
+++ src/components/EventMap/EventMap.js
@@ -10,18 +10,28 @@ function convert(e) {
     day = ("0" + date.getDate()).slice(-2);
   return [day,mnth,date.getFullYear()].join("/");
 }
+// filter the events that are coming from the API without lat and long
+const clearLtLong =(apifiltered)=>{
+  const cordApi=[];
+  apifiltered.map((event)=>{
+    if(event.latitud!== undefined && event.longitud!== undefined ){
+      cordApi.push(event)
+    }
+    
+  })
+  console.log(cordApi)
+ return cordApi
+}
 
 export default class EventMap extends Component {
   // in tha state are the coordinates of Catalunya
   state = {
-    lat: parseFloat(this.props.apiFiltered[0].latitud),
-    lng: parseFloat(this.props.apiFiltered[0].longitud),
+    lat: parseFloat(clearLtLong(this.props.apiFiltered)[0].latitud),
+    lng: parseFloat(clearLtLong(this.props.apiFiltered)[0].longitud),
     zoom: 12
   }
-
-
   render() {
-    //  console.log('gfcgycuh', this.props.apiFiltered)
+     console.log('gfcgycuh', this.props.apiFiltered)
     const position = [this.state.lat, this.state.lng]
     
    //  if the array of the props are empty, to keep the state, only if its filtred render the
@@ -29,6 +39,9 @@ export default class EventMap extends Component {
     //    this.props.apiFiltered.push({latitud:41.5912, longitud:1.5209})
     // };
     
+// if ( (this.props.apiFiltered[0].latitud === '') && (this.props.apiFiltered[0].longitud === '')) {
+//   this.props.apiFiltered.push({latitud:41.5912, longitud:1.5209})
+// };
     // used the parseFloat method to convert from string to inter keeping the decimals
     return (
       <Map center={position} zoom={this.state.zoom}>
@@ -37,7 +50,7 @@ export default class EventMap extends Component {
                 url='https://api.mapbox.com/styles/v1/ceec/ciyxop55w000h2qo9pqyfyy9w/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2VlYyIsImEiOiJjaXl1bWlwZ2QwMW1sMzNxbjMxN3JhdGJlIn0.Y_U5JsNv727e2TqKTJ7gTQ'
               />
               {/* creating a map over the markers, for each markers, that we have declared below, to display a marker on map */}
-        {this.props.apiFiltered.map((event, index) => (
+        {clearLtLong(this.props.apiFiltered).map((event, index) => (
         <Marker key={index} position={[parseFloat(event.latitud),parseFloat(event.longitud)]}>
           <Popup>
            <img width='150' height='100'
