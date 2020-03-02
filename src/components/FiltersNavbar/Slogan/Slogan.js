fcd81e15d596dd8483675005c980090057a77f39
Sandra <alecsandra.serediuc@gmail.com>
2020-03-02 14:43:44 +0100

    Edited a word that was written bad

--- src/components/FiltersNavbar/Slogan/Slogan.js
+++ src/components/FiltersNavbar/Slogan/Slogan.js
@@ -11,7 +11,7 @@ const Slogan = () =>{
       alignItems="center"> 
       <img alt="animate_logo" className='logo_slogan' 
         height='120vmin' display='flex' width='100%' src={logo} />
-      <Grid item xs={12}><h2 className='text_slogan' >Discover Catalunyan events!</h2> </Grid>
+      <Grid item xs={12}><h2 className='text_slogan' >Discover Catalan events!</h2> </Grid>
     </Grid>)
 }
 
